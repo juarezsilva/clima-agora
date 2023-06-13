2ce10105c421fbc99f5dbf09239c41fd2a8b0dec
Juarez Santos <juarez_santos2912@yahoo.com.br>
2023-06-13 19:38:58 -0300

    Atualizando a pasta

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+// Variávies e seleção de elementos
+const apiKey = "ddaaf89284379db73ddc6a7c54a3257f";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search")
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
+
+const weatherContainer = document.querySelector("#weather-data");
+
+// Funções
+const getWeatherData = async(city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    return data;
+};
+
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerHTML = data.name;
+    tempElement.innerHTML = parseInt(data.main.temp);
+    descElement.innerHTML = data.weather[0].description;
+    weatherIconElement.setAttribute("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerHTML = `${data.main.humidity}%`;
+    windElement.innerHTML = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove("hide");
+};
+
+// Eventos
+searchBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e)=> {
+    if (e.code === "Enter") {
+        const city = e.target.value;
+
+        showWeatherData(city);
+    }
+});
+
